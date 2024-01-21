b428aa1f84b6dd8c2f3323bd45ac20c0b1921e33
SUBHAYAN DASGUPTA <52292499+subhayanDsgpt-GitHub@users.noreply.github.com>
2024-01-22 00:43:11 +0530

    Update variables.tf

--- terraform/AWS/variables.tf
+++ terraform/AWS/variables.tf
@@ -1,6 +1,6 @@
 # Author:           Subhayan Dasgupta
 # Date Created:     11/01/2024
-# Date Modified:    14/01/2024
+# Date Modified:    22/01/2024
 
 # Description:
 # Hold the values for the variables which can be parameterized in main.tf.
@@ -8,28 +8,10 @@
 # Usage:
 # variables.tf.
 
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
-  description = "Use secrets.tfvars to pass the secrets securely."
-  default     = ""
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  description = "Use secrets.tfvars to pass the secrets securely."
-  default     = ""
-}
-
 variable "name" {
   type        = string
   description = "Use the following default string as a prefix for the AWS EKS cluster name."
   default     = "PoCKubernetesCluster"
-}
-
-variable "region" {
-  type        = string
-  description = "Choose a region to deploy your AWS resources." # Need User input to proceed for deployments.
-  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
